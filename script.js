b971c7096dfd3bf47a03587a643089271cad7725
bia-pequeno <bia.2017pequeno@gmail.com>
2022-01-19 00:48:20 -0300

    atividade concluida

---
+++ script.js
@@ -0,0 +1,23 @@
+for (let cont = 1; 10 >= cont; cont +=1){
+    console.log(cont)
+    
+}
+
+let contador = 10
+
+while ( 1 <=contador){
+    console.log(contador)
+    contador -=1
+}
+
+for (let i =1; 100 >= i; i +=2){
+    console.log(i)
+    
+}
+
+let cont2 = 2
+
+while (100 >=cont2){
+    console.log(cont2)
+    cont2 +=2
+}
